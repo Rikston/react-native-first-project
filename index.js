a403f6afb7dedf65899f692b85e4439b562b043f
Roman Panas <rpanas@Romans-iMac.local>
2018-12-14 18:49:27 +0200

    add version

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 /** @format */
 
 import { AppRegistry } from 'react-native';
-import App from './App';
-// import App from './src/screen/index';
+// import App from './App';
+import App from './src/screen/index';
 import { name as appName } from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
