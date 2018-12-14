9553928a93b645506809cd219c751618b5f5ac72
Roman Panas <rpanas@Romans-iMac.local>
2018-12-14 19:18:36 +0200

    deleted comment from App.js

--- App.js
+++ App.js
@@ -7,35 +7,6 @@ import Two from './src/screen1';
 import Three from './src/screen2';
 import { BLUE } from './src/services/constants';
 
-// const tab = createBottomTabNavigator(
-//   {
-//     Stargate: One,
-//     Batman: Two,
-//     Spiderman: Three
-//   },
-//   {
-//     defaultNavigationOptions: ({ navigation }) => ({
-//       tabBarIcon: ({ focused, tintColor }) => {
-//         const { routeName } = navigation.state;
-//         let iconName;
-//         console.log(routeName);
-//         if (routeName === 'Stargate') {
-//           iconName = focused ? 'ios-videocam' : 'ios-play';
-//         } else if (routeName === 'Batman') {
-//           iconName = focused ? 'ios-videocam' : 'ios-play';
-//         } else if (routeName === 'Spiderman') {
-//           iconName = focused ? 'ios-videocam' : 'ios-play';
-//         }
-//         return <Ionicons name={iconName} size={25} color={tintColor} />;
-//       }
-//     }),
-//     tabBarOptions: {
-//       activeTintColor: 'tomato',
-//       inactiveTintColor: 'gray'
-//     }
-//   }
-// );
-
 const sideMenu = createDrawerNavigator(
   {
     Screen1: {
